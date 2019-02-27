959c6193dc7d0edafc18691837ae2b5b1cae1d28
ZenDranZer <vorasarvesh99@gmail.com>
2019-02-26 19:32:12 -0500

    JGraphT lib.

--- src/utils/GraphUtil.java
+++ src/utils/GraphUtil.java
@@ -5,7 +5,12 @@ import models.GameCountry;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+
 
 /**GraphUtil class is used for representing the map in a graph formation
  * and perform graph operations like graph traversal, connectivity, etc.
@@ -21,13 +26,12 @@ public class GraphUtil {
     }
 
     /**Getter method to access the countryGraph
-     * @return Graph<GameCountry> an object of */
+     * @return Graph<GameCountry> an object which */
     public Graph<GameCountry, DefaultEdge> getCountryGraph() {
         return countryGraph;
     }
 
     public void setCountryGraph(ArrayList<GameCountry> countryList) {
-
 
         for (GameCountry gameCountry : countryList) {
             countryGraph.addVertex(gameCountry);
@@ -40,4 +44,17 @@ public class GraphUtil {
             }
         }
     }
+
+    public boolean areConnected(GameCountry firstCountry,GameCountry secondCountry){
+        return countryGraph.containsEdge(firstCountry, secondCountry);
+    }
+
+    public Iterator<GameCountry> breadthFirstSearch(GameCountry initialCountry){
+        return new BreadthFirstIterator<>(countryGraph);
+    }
+
+    public Iterator<GameCountry> depthFirstSearch(GameCountry initialCountry){
+        return new DepthFirstIterator<>(countryGraph);
+    }
+
 }
