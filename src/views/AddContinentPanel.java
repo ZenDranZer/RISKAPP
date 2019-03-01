50b96bb21e3019b55a745b20d2bf292f185f35d2
NaghmehAnsari <naghmeh234@gmail.com>
2019-03-01 17:55:59 -0500

    remove and add continent panels added

---
+++ src/views/AddContinentPanel.java
@@ -0,0 +1,98 @@
+package views;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AddContinentPanel extends JPanel {
+    public AddContinentPanel() {
+        initComponents();
+    }
+
+    private void addContinentMouseClicked(MouseEvent e) {
+        String continentName = nameField.getText();
+        int value = Integer.parseInt(valueField.getText());
+
+    }
+
+    private void backButtonMouseClicked(MouseEvent e) {
+        // TODO add your code here
+    }
+
+    private void initComponents() {
+        label1 = new JLabel();
+        label2 = new JLabel();
+        nameField = new JTextField();
+        label3 = new JLabel();
+        valueField = new JTextField();
+        addContinent = new JButton();
+        backButton = new JButton();
+
+        //======== this ========
+
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+        //---- label1 ----
+        label1.setText("Create Continent");
+        add(label1, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- label2 ----
+        label2.setText("Enter Continent Name:");
+        add(label2, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+        add(nameField, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- label3 ----
+        label3.setText("Enter Value:");
+        add(label3, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- valueField ----
+        valueField.setText("1");
+        add(valueField, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- addContinent ----
+        addContinent.setText("Add Continent");
+        addContinent.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                addContinentMouseClicked(e);
+            }
+        });
+        add(addContinent, new GridBagConstraints(2, 4, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- backButton ----
+        backButton.setText("Back");
+        backButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                backButtonMouseClicked(e);
+            }
+        });
+        add(backButton, new GridBagConstraints(2, 5, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+    }
+
+    private JLabel label1;
+    private JLabel label2;
+    private JTextField nameField;
+    private JLabel label3;
+    private JTextField valueField;
+    private JButton addContinent;
+    private JButton backButton;
+}
