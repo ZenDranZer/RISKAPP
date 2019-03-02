53f27a65854fa5ecb8198fa7fe1cd892d2172525
NaghmehAnsari <naghmeh234@gmail.com>
2019-03-01 20:26:48 -0500

    remove country panel added

---
+++ src/views/RemoveCountryPanel.java
@@ -0,0 +1,81 @@
+package views;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RemoveCountryPanel extends JPanel {
+    public RemoveCountryPanel() {
+        initComponents();
+    }
+
+    private void backButtonMouseClicked(MouseEvent e) {
+        // TODO add your code here
+    }
+
+    private void removeButtonMouseClicked(MouseEvent e) {
+        // TODO add your code here
+    }
+
+    private void initComponents() {
+       
+        label1 = new JLabel();
+        label2 = new JLabel();
+        textField1 = new JTextField();
+        backButton = new JButton();
+        removeButton = new JButton();
+
+        //======== this ========
+
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 161, 0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+        //---- label1 ----
+        label1.setText("Remove Country");
+        add(label1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- label2 ----
+        label2.setText("Enter Country Name:");
+        add(label2, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+        add(textField1, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+        //---- backButton ----
+        backButton.setText("Back");
+        backButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                backButtonMouseClicked(e);
+            }
+        });
+        add(backButton, new GridBagConstraints(3, 4, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 5), 0, 0));
+
+        //---- removeButton ----
+        removeButton.setText("Remove");
+        removeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                removeButtonMouseClicked(e);
+            }
+        });
+        add(removeButton, new GridBagConstraints(4, 4, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
+    }
+
+    private JLabel label1;
+    private JLabel label2;
+    private JTextField textField1;
+    private JButton backButton;
+    private JButton removeButton;
+}
