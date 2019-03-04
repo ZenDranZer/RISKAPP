7ccbad4a5ca1e2999c1421ff5bdcd9de79d748ab
jil <mehtajil131@gmail.com>
2019-03-04 02:16:21 -0500

    Added test cases for player class

--- src/Test/TurnControllerTest.java
+++ src/Test/TurnControllerTest.java
@@ -1,22 +1,54 @@
 package Test;
 
+import controllers.TurnController;
 import models.Player;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import utils.Constants;
-import utils.TestUtil;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class TurnControllerTest {
+    private ArrayList<Player> players;
+
 
     @Before
     public void setUp() throws Exception {
-        Player p = new Player();
+        players = new ArrayList<Player>();
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test function to test if the total number of armies with the player countires
+     * is same as that assigned to the player initially
+     * @throws Exception
+     */
+    @Test
+    public void testPlayerArmies() throws Exception {
+        /*Check if the number of armies possessed by the player is not more than the
+          initial number of armies assigned to the player*/
+        TurnController t = new TurnController();
+        for(int i=0 ; i<players.size();i++) {
+            assertEquals(t.getEachPlayerArmy(players),players.get(i).getPlayerArmies());
+        }
+    }
+
+    /**
+     * Test function to test if each continent has atleast 1 army stationed on it
+     * @throws Exception
+     */
+    @Test
+    public void testInitialArmiesContinent() throws Exception {
+        /* Check if all the continents contained by the players has atleast 1 army assigned to it */
+        for(int i=0;i<players.size();i++) {
+            for (int j=0; j<players.get(i).getCountries().size();j++) {
+                assertTrue(players.get(i).getCountries().get(i).getArmiesStationed() > 1);
+            }
+        }
     }
 }
