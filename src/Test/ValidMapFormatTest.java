4ede14e4ad0d17e99a6a4140b0e43dd9823f23af
NaghmehAnsari <naghmeh234@gmail.com>
2019-03-04 14:46:18 -0500

    ValidMapFormat test added

---
+++ src/Test/ValidMapFormatTest.java
@@ -0,0 +1,33 @@
+package Test;
+
+import controllers.MapGenerator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ValidMapFormatTest {
+
+    MapGenerator mapGenerator;
+    @Before
+    public void setUp() throws Exception {
+         mapGenerator = new MapGenerator();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testValidMapFormat() throws IOException {
+
+        String output = mapGenerator.readConquestFile("/home/naghmeh/IdeaProjects/RISKAPP_new/src/mapFiles/Africa.map");
+        assertEquals(output,"SUCCESS");
+    }
+
+
+}
