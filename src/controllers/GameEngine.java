de6dbef8e24d3c1f049dd20e44329ee7974a0326
jil <mehtajil131@gmail.com>
2019-03-05 14:36:03 -0500

    Removing CUI methods

--- src/controllers/GameEngine.java
+++ src/controllers/GameEngine.java
@@ -43,27 +43,6 @@ public class GameEngine {
 		return mapGenerator;
 	}
 
-	// CUI
-	public void getPlayerInfo(ArrayList<Player> activePlayers) {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter number of players");
-		int noOfPlayers = keyboard.nextInt();
-		if (noOfPlayers < 2) {
-			System.out.println("Minimum number of players is 2");
-		} else if (noOfPlayers > 5) {
-			System.out.println("Maximum number of players is 5");
-		} else {
-			for (int i = 1; i <= noOfPlayers; i++) {
-				Player p = new Player();
-				System.out.println("Enter player name " + i + "\n");
-				p.setId(i);
-				p.setName(keyboard.next());
-				listActivePlayers.add(p);
-			}
-		}
-		keyboard.close();
-	}
-
 	/**
 	 * Gets the file path of the current map
 	 * @return String absolute path of the map file
@@ -80,22 +59,6 @@ public class GameEngine {
 		this.mapPath = mapPath;
 	}
 
-	// cui
-	/**
-	 * This method takes in the number of players and their names
-	 *
-	 * @return: list of player objects
-	 */
-	public ArrayList<Player> getInitialPlayers() {
-
-		try {
-			getPlayerInfo(listActivePlayers);
-		} catch (Exception ex) {
-			System.out.println("Error getting player data");
-		}
-		return listActivePlayers;
-	}
-
 	/**
 	 * function to initialize the and set the initial players for the game
 	 * 
@@ -110,11 +73,6 @@ public class GameEngine {
 			i++;
 		}
 	}
-	
-	//CUI
-/*	public void initialise() {
-		getInitialPlayers();
-	}*/
 
 	/**
 	 * Initialize the game engine with the initial set of players, randomly
