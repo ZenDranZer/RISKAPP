213a135ad2406778f7113336130e989a2cc1f40a
Sidhant Gupta <sidhant.gupta.hsp@gmail.com>
2019-03-13 13:35:44 -0400

    Updated existing models and added new models for build 2

---
+++ src/models/GameState.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.util.*;
+
+/**
+ * Class that represents and holds the data for the GamePlay
+ * Contains all the players and the map
+ */
+public class GameState {
+
+	private ArrayList<Player> lstPlayers;
+	private String mapPath;
+	private HashMap<String, RiskCard> cardPile;
+
+	public ArrayList<Player> getPlayers() {
+		return lstPlayers;
+	}
+
+	public void setPlayers(ArrayList<Player> lstPlayers) {
+		this.lstPlayers = lstPlayers;
+	}
+
+	public String getMapPath() {
+		return mapPath;
+	}
+
+	public void setMapPath(String mapPath) {
+		this.mapPath = mapPath;
+	}
+
+	public HashMap<String, RiskCard> getCardPile() {
+		return cardPile;
+	}
+
+	public void setCardPile(HashMap<String, RiskCard> cardPile) {
+		this.cardPile = cardPile;
+	}
+
+}
