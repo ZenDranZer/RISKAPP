91563b2801cdf75dc03720348624422535645296
NaghmehAnsari <naghmeh234@gmail.com>
2019-04-04 21:12:16 -0400

    update cheater player

--- src/models/CheaterPlayer.java
+++ src/models/CheaterPlayer.java
@@ -8,6 +8,46 @@ public class CheaterPlayer extends Player {
 
     }
     CheaterPlayer(String name, int id, GameMap gameMap){
-        super();
+        super(name,id,gameMap);
+    }
+
+    @Override
+    public void reinforcement(String countryName, int armies) {
+        for (GameCountry country: this.countries) {
+            country.setArmies(country.getArmiesStationed() * 2);
+        }
+
+    }
+
+    @Override
+    public String attack(Player defender, GameCountry attackingCountry, GameCountry defendingCountry, int redDice, int whiteDice) {
+        String status = "";
+        defender.removeCountry(defendingCountry);
+        this.setCountries(defendingCountry);
+        this.updateContinents();
+        defendingCountry.setArmies(attackingCountry.getArmiesStationed() - 1);
+        attackingCountry.removeArmies(attackingCountry.getArmiesStationed() - 1);
+        status = "success";
+        if (defender.countries.size() == 0) {
+            this.getCardsHeld().addAll(defender.getCardsHeld());
+            eliminate(defender);
+            status = "eliminated";
+            if (hasPlayerWon(this)) {
+                status = "winner";
+            }
+        }
+        return status;
+    }
+
+    @Override
+    public void fortify(String countryToFortify, String fortifyFrom, int armies) {
+
+        GameCountry toCountry = countries.stream().filter(cntry -> cntry.getCountryName().equals(countryToFortify))
+                .findFirst().get();
+        GameCountry fortifyingCountry = countries.stream().filter(cntry -> cntry.getCountryName().equals(fortifyFrom))
+                .findFirst().get();
+
+        toCountry.setArmies(toCountry.getArmiesStationed() + fortifyingCountry.getArmiesStationed()*2);
+        //fortifyingCountry.setArmies(fortifyingCountry.getArmiesStationed() - armies);
     }
 }
