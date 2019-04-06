4188a5dbedeb896ed0f19b51d3be10c186852e03
NaghmehAnsari <naghmeh234@gmail.com>
2019-04-06 13:50:59 -0400

    Benevolent player find week country method added

--- src/models/BenevolentPlayer.java
+++ src/models/BenevolentPlayer.java
@@ -7,4 +7,17 @@ public class BenevolentPlayer extends Player {
     BenevolentPlayer(String name, int id, GameMap gameMap){
         super();
     }
+
+    public GameCountry findWeekCountry(){
+
+        GameCountry week = this.countries.get(0);
+        int leastNumberOfArmies = this.countries.get(0).getArmiesStationed();
+        for (GameCountry country: this.countries) {
+            if (country.getArmiesStationed() < leastNumberOfArmies){
+                week =country;
+            }
+        }
+
+        return week;
+    }
 }
