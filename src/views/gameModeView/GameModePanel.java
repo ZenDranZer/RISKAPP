368405f59672cc9e7fc65496c81cdda4c273602e
jil <mehtajil131@gmail.com>
2019-04-08 16:34:41 -0400

    Added Load Button.

--- src/views/gameModeView/GameModePanel.java
+++ src/views/gameModeView/GameModePanel.java
@@ -46,18 +46,23 @@ public class GameModePanel extends JPanel {
         parent.setVisible(true);
     }
 
+    private void loadGameButtonMouseClicked(MouseEvent e) {
+        // TODO add your code here
+    }
+
     private void initComponents() {
         singleModeButton = new JButton();
         tournamentModeButton = new JButton();
+        loadGameButton = new JButton();
         backButton = new JButton();
 
         //======== this ========
 
         setLayout(new GridBagLayout());
         ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0};
-        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0};
         ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
-        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 1.0E-4};
 
         //---- singleModeButton ----
         singleModeButton.setText("Single Game Mode");
@@ -83,6 +88,18 @@ public class GameModePanel extends JPanel {
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 5, 5), 0, 0));
 
+        //---- loadGameButton ----
+        loadGameButton.setText("Load Game");
+        loadGameButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                loadGameButtonMouseClicked(e);
+            }
+        });
+        add(loadGameButton, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 5), 0, 0));
+
         //---- backButton ----
         backButton.setText("Back");
         backButton.addMouseListener(new MouseAdapter() {
@@ -98,5 +115,6 @@ public class GameModePanel extends JPanel {
 
     private JButton singleModeButton;
     private JButton tournamentModeButton;
+    private JButton loadGameButton;
     private JButton backButton;
 }
