c5f603870b70189429dad87dcf617b5a7d69fc16
NaghmehAnsari <naghmeh234@gmail.com>
2019-04-09 20:35:52 -0400

    cheater test case update

--- src/Test/TestCheaterPlayer.java
+++ src/Test/TestCheaterPlayer.java
@@ -23,6 +23,7 @@ public class TestCheaterPlayer {
     GameState gameState;
     @Before
     public void setup(){
+
         gameMap = new GameMap();
         gameContinent =new GameContinent();
         gameContinent.setContinentName("Asia");
@@ -34,16 +35,18 @@ public class TestCheaterPlayer {
         arrayList.add(riskCard);
 
         attackingCountry1 = new GameCountry();
-        attackingCountry1.setArmies(1);
+        attackingCountry1.setArmies(5);
         attackingCountry1.setCountryName("IRAN");
         attackingCountry1.setContinent(gameContinent);
         attackingCountry1.setArmies(2);
         attackingCountry1.setArmies(3);
         defendingCountry1 = new GameCountry();
-        defendingCountry1.setArmies(1);
+        defendingCountry1.setArmies(6);
         defendingCountry1.setCountryName("INDIA");
         defendingCountry1.setContinent(gameContinent);
-        defendingCountry1.setCurrentPlayer(defender);
+
+        attackingCountry1.addNeighbouringCountry(defendingCountry1);
+        defendingCountry1.addNeighbouringCountry(attackingCountry1);
         //---
         attackingCountry2 = new GameCountry();
         attackingCountry2.setArmies(1);
@@ -55,32 +58,55 @@ public class TestCheaterPlayer {
         defendingCountry2.setArmies(1);
         defendingCountry2.setCountryName("CHINA");
         defendingCountry2.setContinent(gameContinent);
-        defendingCountry2.setCurrentPlayer(defender);
+
+        attackingCountry1.addNeighbouringCountry(defendingCountry1);
+        attackingCountry1.addNeighbouringCountry(defendingCountry2);
+        defendingCountry1.addNeighbouringCountry(attackingCountry1);
+        defendingCountry2.addNeighbouringCountry(attackingCountry1);
 
         gameMap.countryHashMap.put(attackingCountry1.getCountryName() , attackingCountry1);
         gameMap.countryHashMap.put(defendingCountry1.getCountryName() , defendingCountry1);
         gameMap.countryHashMap.put(attackingCountry2.getCountryName() , attackingCountry2);
         gameMap.countryHashMap.put(defendingCountry2.getCountryName() , defendingCountry2);
+        gameMap.continentHashMap.put(gameContinent.getContinentName() , gameContinent);
+
+
+        gameState = new GameState();
+        gameState.setActivePlayer(attacker);
+
+
+        gameContinent.setCountries(attackingCountry1);
+        gameContinent.setCountries(attackingCountry2);
+        gameContinent.setCountries(defendingCountry1);
+        gameContinent.setCountries(defendingCountry2);
+
+        gameMap.addCountry(attackingCountry1);
+        gameMap.addCountry(attackingCountry2);
+        gameMap.addCountry(defendingCountry1);
+        gameMap.addCountry(defendingCountry2);
         attacker = new CheaterPlayer("Naghmeh" , 1 , gameMap);
         defender = new Player("Lin" , 2, gameMap);
-
+        defender.setContinents(gameContinent);
+        attacker.setCountries(attackingCountry1);
+        System.out.println(attackingCountry1.getNeighbouringCountries().size() + "333");
+        defender.setPlayerArmies(3);
         attacker.addRiskCard(riskCard);
         defender.addRiskCard(riskCard);
         attacker.setCardsHeld(arrayList);
         attacker.setPlayerArmies(1);
         attacker.setPlayerArmies(2);
-        attacker.setPlayerArmies(3);
+        attacker.setPlayerArmies(7);
         defender.setCardsHeld(arrayList);
+        attacker.setContinents(gameContinent);
         attackingCountry1.setCurrentPlayer(attacker);
-        gameState = new GameState();
-        gameState.setActivePlayer(attacker);
-        defender.setPlayerArmies(1);
+        defendingCountry2.setCurrentPlayer(defender);
+        defendingCountry1.setCurrentPlayer(defender);
 
     }
 
     @Test
     public void TestAttackFunctionRedDice() {
-        assertEquals( "Winner" , attacker.attack());
+        assertEquals( "success|" , attacker.attack());
 
     }
 }
